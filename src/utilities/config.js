51e3637d0b061c48932777eed86e4d1c87fd7135
usamamanzoor991 <usamamanzoor991@gmail.com>
2026-04-15 20:01:12 +0500

    update(Devnet to mainnet)

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -7,4 +7,7 @@ export const contract_address = "0x321a991f974616adDFDea9281Cdb3F39faa4537C";
 
 export const PRESALE_PROGRAM_ID = "presSVxnf9UU8jMxhgSMqaRwNiT36qeBdNeTRKjTdbj";
 
-export const PRESALE_VAULT_PDA = import.meta.env.VITE_PDA;
+export const PRESALE_VAULT_PDA =
+  network == "devnet"
+    ? import.meta.env.VITE_PDA
+    : import.meta.env.VITE_PDA_MAINNET;
